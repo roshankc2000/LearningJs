7af0f6fbccbd18e17e5249aa5581f738037f2609
Roshan K.C <officialroshankc@gmail.com>
2023-01-12 18:29:05 +0545

    passWord Generator v1.0

---
+++ day_9 OOP/oop.js
@@ -0,0 +1,16 @@
+//ch8 qns all
+class complexNumber{
+    constructor(real,comp) {
+        this.real = real;
+        this.comp = comp;
+
+    }
+    realPart() {
+        //opeartions
+    }
+    compPart() {
+        //operations
+    }
+}
+let num = new complexNumber(5,6);
+
